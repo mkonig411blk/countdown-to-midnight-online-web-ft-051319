0977a8914ad77754d24192f00498a38f705d3a5f
Margot Konig <margot.konig@blackrock.com>
2019-05-15 02:06:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  if number == 0
+  puts "HAPPY NEW YEAR!"
+else 
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+end
+end
+
+def countdown_with_sleep(number)
+  
 end
